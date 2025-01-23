1a74194ae4a9038b7a62ab257a15e18bd8dccda8
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2025-01-24 02:07:27 +0500

    Update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -22,7 +22,7 @@ export default function SignIn() {
     setLoading(true);
 
     try {
-      const response = await fetch("https://crm-taskback.vercel.app/api/users/signIn", {
+      const response = await fetch("https://crm-task-backend-1.onrender.com/api/users/signIn", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
