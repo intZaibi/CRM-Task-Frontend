e70f2bca631abf9acbab31d6ef8c415330cf912b
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2025-01-24 01:48:47 +0500

    Update leadServices.jsx

--- src/services/leadServices.jsx
+++ src/services/leadServices.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://crm-taskback.vercel.app/api';
 
 const getLeads = async () => {
   try {
